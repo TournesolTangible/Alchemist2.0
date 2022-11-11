ed218def65c527d0eb5cc72a30ad279e4f3674e7
TournesolTangible <99299545+TournesolTangible@users.noreply.github.com>
2022-11-10 19:12:32 -0600

    Added Character Switch UI to Start screen

--- Alchemist/Assets/Scripts/CharacterSwitcher.cs
+++ Alchemist/Assets/Scripts/CharacterSwitcher.cs
@@ -1,29 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSwitcher : MonoBehaviour
 {
+    // To hold the sprites to switch through
+    public Sprite[] Sprites;
 
-    public Image Hat1;
-    public Image Hat2;
-    public Image Hat3;
-    public Image Hood1;
-    public Image Hood2;
-    public Image Hood3;
+    // To hold the current Sprite
+    private Sprite Current;
+    private int Index;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Image[] Hats = new Array(3);
-        Image[] Hoods = new Array(3);
+    public Image Bilby;
 
-        
+    // Buttons for switching character
+    public Button OutfitButton;
+    public Button ToneButton;
+
+    public void Start() {
+        Index = 0;
+        Current = Sprites[Index];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // To switch Outfit
+    public void FireOutfit() {
+        if (Index < 3) {
+            Index += 3;
+        } else {
+            Index -= 3;
+        }
+        print(Index);
+        Current = Sprites[Index];
+        Bilby.sprite = Current;
     }
+
+    // To switch Tone
+    public void FireTone() {
+        if (Index == 2 || Index == 5) {
+            Index -= 2;
+        } else {
+            Index += 1;
+        }
+        print(Index);
+        Current = Sprites[Index];
+        Bilby.sprite = Current;
+    }
+
 }
