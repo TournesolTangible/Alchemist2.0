5fb54b9c219df95c48461a3968bc9dc7a6b9d8bb
stephen <swpals@gmail.com>
2022-11-13 19:18:03 -0600

    Enemies move with level parts
    enemies and trees move with level parts and bilby dies (sometimes) when hitting the left side of the screen

--- Alchemist/Assets/Scripts/MoveWithLevel.cs
+++ Alchemist/Assets/Scripts/MoveWithLevel.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class MoveWithLevel : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.CompareTag("Player")) {
-            collision.transform.SetParent(this.transform);
-        }
-    }
+    //private void OnTriggerEnter2D(Collider2D collision) {
+    //    if (collision.CompareTag("Enemy")) {
+    //        collision.transform.SetParent(this.transform);
+    //    }
+    //}
 
-    private void OnTriggerExit2D(Collider2D collision) {
-        if (collision.CompareTag("Player")) {
-            collision.transform.SetParent(null);
-        }
-    }
+    //private void OnTriggerExit2D(Collider2D collision) {
+    //    if (collision.CompareTag("Enemy")) {
+    //        collision.transform.SetParent(null);
+    //    }
+    //}
 }
