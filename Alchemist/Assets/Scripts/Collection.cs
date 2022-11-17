35d66f956402e7db4b6c14b2925586e075b37dd3
stephen <swpals@gmail.com>
2022-11-17 16:45:34 -0600

    AmazingAnimations
    the best damn animations you have ever seen, like at least shrek quality

--- Alchemist/Assets/Scripts/Collection.cs
+++ Alchemist/Assets/Scripts/Collection.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class Collection : MonoBehaviour {
 
     private float speed = 7;
+    
+    [SerializeField] private GameManager gm;
 
     public void OnTriggerStay2D(Collider2D collider) {
 
         if (collider.CompareTag("Player")) {
 
-    
+            // if hits boxCollider destroy object (will need to add where to send collection here as well)
             if (collider is BoxCollider2D) {
-                Debug.Log("hitting box collider 2d");
+                
                 Destroy(this.gameObject);
             }
-            if (collider is CircleCollider2D) {
 
+            // pull towards player if in the circleCollider
+            if (collider is CircleCollider2D) {
+                // speed at which magnet pulls
                 float step = speed * Time.deltaTime;
                 // move sprite towards the target location
                 transform.position = Vector2.MoveTowards(transform.position, collider.transform.position, step); 
