fee4196e3b182630651975f6371a3ba69696ebce
TournesolTangible <99299545+TournesolTangible@users.noreply.github.com>
2022-11-17 18:14:45 -0600

    Added hearts - player takes one heart of damage on every enemy collision

---
+++ Alchemist/Assets/Scripts/HurtPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayer : MonoBehaviour
+{
+    [SerializeField] private float _damageTime;        // Time at which the enemy can do damage again
+    [SerializeField] private float _damageRate = 2.0f; // Time between refraction periods (between attacks)
+    
+    void OnTriggerStay2D(Collider2D other) {
+
+        if (other.transform.tag == "Player" && other is CapsuleCollider2D && Time.time > _damageTime) {
+            other.transform.GetComponent<PlayerStats>().TakeDamage(1);
+            _damageTime = Time.time + _damageRate;
+        }
+    }
+
+
+
+            
+
+
+}
