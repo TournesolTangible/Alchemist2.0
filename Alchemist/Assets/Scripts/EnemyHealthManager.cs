8c8aa951a0e6a7591784600439431b6e399fa288
stephen <swpals@gmail.com>
2022-11-17 21:09:53 -0600

    Basic attack and  enemy health
    basic attack (every second) in facing direction, and enemy health manager scripts

---
+++ Alchemist/Assets/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour
+{
+   private float enemyMaxHealth = 3.0f;
+   private float enemyCurrentHealth;
+
+   [SerializeField] private GameObject collectable;
+
+   void Start() {
+    enemyCurrentHealth = enemyMaxHealth;
+   }
+
+   void Update() {
+    if (enemyCurrentHealth <= 0) {
+        Destroy(this.gameObject);
+        Instantiate(collectable, transform.position, transform.rotation);
+    }
+   }
+
+   // take damage
+   public void TakeDamage(float damage) {
+    enemyCurrentHealth -= damage;
+   }
+
+}
