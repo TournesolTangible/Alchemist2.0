7c2227171ae02cca06ce96e8fedc03340ebe21a1
symphonygl <roa.lindsay@usask.ca>
2022-11-20 20:31:00 -0600

    Revert "Merge branch 'CheckpointShopBranch'"
    This reverts commit 0896b5868ecdfc0e45130df3e5d289816d1bb97f, reversing
    changes made to 71f62458d1755cef1b59236fe1a16c15bdfec9c2.

--- Alchemist/Assets/Scripts/GameManager.cs
+++ Alchemist/Assets/Scripts/GameManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,37 +8,33 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance = null;
 
     public GameObject Player;
-    public GameObject checkpointShop;
-    public GameObject pauseMenu;
+    public ShopScreen ShopScreen;
 
-    [SerializeField] public float playerHealth = 5.0f;
-    [SerializeField] public float playerStrength = 5.0f; // adjust
-    [SerializeField] public float playerLuck = 5.0f; // adjust
-    [SerializeField] public float playerPeace = 5.0f; // adjust
-    [SerializeField] public float playerProtection = 5.0f; // adjust
-    [SerializeField] public float playerAlchemy = 8.0f;
-
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        if (Input.GetKey(KeyCode.C)) {
-            checkpointShop.GetComponent<CheckpointShop>().OpenCheckpointShop();
+        if (Instance == null)
+        {
+            Instance = this;
         }
 
-        if (Input.GetKey("escape")) {
-            pauseMenu.GetComponent<PauseMenu>().OpenPauseMenu();
-        }
-        
-    }
-
-    void Awake() {
-
-        if (instance == null) {
-            instance = this;
-        }
-
-        else if (instance != this) {
+        else if (Instance != this)
+        {
             Destroy(gameObject);
         }
     }
+
+    public void RunShopScreen() {
+        ShopScreen.Setup();
+    }
+
+    // to test the shop
+    void Update() {
+        if (Input.GetKey("o")) {
+            ShopScreen.Setup();
+        }
+        if (Input.GetKey("p")) {
+            ShopScreen.Exit();
+        }
+    }
+
 }
