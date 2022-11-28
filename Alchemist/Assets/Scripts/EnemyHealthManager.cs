7bab6dd40c2f13a3bae9498b96ea362aaf299d12
stephen <swpals@gmail.com>
2022-11-28 13:02:25 -0600

    randomized drops

--- Alchemist/Assets/Scripts/EnemyHealthManager.cs
+++ Alchemist/Assets/Scripts/EnemyHealthManager.cs
@@ -16,7 +16,17 @@ public class EnemyHealthManager : MonoBehaviour
    void Update() {
     if (enemyCurrentHealth <= 0) {
         Destroy(this.gameObject);
-        Instantiate(collectable, transform.position, new Quaternion(0,0,0,0));
+        int random_number = Random.Range(0,10); // ganerates a random number between 1 to 10
+        if (10 == random_number) { // if roll is 10 get 3 collectables
+            Instantiate(collectable, transform.position, new Quaternion(0,0,0,0));
+        }
+        if (7 < random_number) { // if roll is greater than 7 get 2 collectables
+            Instantiate(collectable, transform.position, new Quaternion(0,0,0,0));
+        }
+        if (4 < random_number) { // if roll is greater than 4 get 1 collectable
+            Instantiate(collectable, transform.position, new Quaternion(0,0,0,0));
+        }
+        // if roll is 4 or less get nothing
     }
    }
 
