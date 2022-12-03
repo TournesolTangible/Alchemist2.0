5891249018c63bd2635b5e90c023778eacfc350b
symphonygl <roa.lindsay@usask.ca>
2022-12-03 02:04:22 -0600

    Added heart drop to all enemies at fixed chance (doesn't collect yet)

--- Alchemist/Assets/Scripts/EnemyHealthManager.cs
+++ Alchemist/Assets/Scripts/EnemyHealthManager.cs
@@ -8,6 +8,7 @@ public class EnemyHealthManager : MonoBehaviour
    private float enemyCurrentHealth;
 
    [SerializeField] private GameObject collectable;
+   [SerializeField] private GameObject heart;
 
    // Reference to GameManager to moderate drop amount (stats)
    private static GameObject GameManager;
@@ -25,7 +26,7 @@ public class EnemyHealthManager : MonoBehaviour
         Destroy(this.gameObject);
         int random_number = Random.Range(0,10); // ganerates a random number between 1 to 10
         random_number += GameManager.GetComponent<GameManager>().playerLuck; // Add player luck to 'random_number'
-        if (random_number == 10) { // if roll is == 10 get 3 collectables
+        if (random_number >= 10) { // if roll is >= 10 get 3 collectables
             Instantiate(collectable, transform.position, new Quaternion(0,0,0,0));
         }
         if (7 < random_number) { // if roll is greater than 7 get 2 collectables
@@ -33,8 +34,13 @@ public class EnemyHealthManager : MonoBehaviour
         }
         if (4 < random_number) { // if roll is greater than 4 get 1 collectable
             Instantiate(collectable, transform.position, new Quaternion(0,0,0,0));
+        } // if roll is 4 or less get nothing
+        
+        // Small chance of heart drop
+        int random_number_two = Random.Range(0,5);
+        if (random_number_two >= 4) {
+            Instantiate(heart, transform.position, new Quaternion(0,0,0,0));
         }
-        // if roll is 4 or less get nothing
     }
    }
 
