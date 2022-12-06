e736a68652ed68026662b8693405b89cc8c26f75
7ElEvan <emk202@mail.usask.ca>
2022-12-06 16:12:06 -0600

    Added sprites for the enviro items and set up the trees to drop acorns

---
+++ Alchemist/Assets/Scripts/TreeDrop.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeDrop : MonoBehaviour
+{
+    [SerializeField] private float enemyMaxHealth = 3.0f;
+    private float enemyCurrentHealth;
+
+    [SerializeField] private GameObject collectable;
+    [SerializeField] private GameObject heart;
+
+    // Reference to GameManager to moderate drop amount (stats)
+    private static GameObject GameManager;
+
+    private bool dropped = false;
+
+    void Awake()
+    {
+        GameManager = GameObject.Find("GameManager");
+    }
+
+    void Start()
+    {
+        enemyCurrentHealth = enemyMaxHealth;
+    }
+
+    void Update()
+    {
+        if (enemyCurrentHealth <= 0)
+        {
+            int random_number = Random.Range(0, 10); // ganerates a random number between 1 to 10
+            random_number += GameManager.GetComponent<GameManager>().playerLuck; // Add player luck to 'random_number'
+            if (random_number >= 10 && !dropped)
+            { // if roll is >= 10 get 3 collectables
+                Instantiate(collectable, transform.position, new Quaternion(0, 0, 0, 0));
+                dropped = true;
+            }
+            if (7 < random_number && !dropped)
+            { // if roll is greater than 7 get 2 collectables
+                Instantiate(collectable, transform.position, new Quaternion(0, 0, 0, 0));
+                dropped = true;
+            }
+            if (4 < random_number && !dropped)
+            { // if roll is greater than 4 get 1 collectable
+                Instantiate(collectable, transform.position, new Quaternion(0, 0, 0, 0));
+                dropped = true;
+            } // if roll is 4 or less get nothing
+            else
+            {
+                dropped = true;
+            }
+
+        }
+    }
+
+    // take damage
+    public void TakeDamage(float damage)
+    {
+        enemyCurrentHealth -= damage;
+        GameManager.GetComponent<GameManager>().countdownCanvas.GetComponent<CheckpointTimer>().ReduceCountdown();
+    }
+
+}
+
