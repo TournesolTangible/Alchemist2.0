07f31aca52772d8e6fe68080b348d8db40af2487
symphonygl <roa.lindsay@usask.ca>
2022-12-06 00:38:19 -0600

    all level objects have environment texture

--- Alchemist/Assets/Scripts/ShootFireballs.cs
+++ Alchemist/Assets/Scripts/ShootFireballs.cs
@@ -12,6 +12,7 @@ public class ShootFireballs : MonoBehaviour
 
     private Vector2 target;
     private bool collidingWithEnemy;
+    private bool upgraded;
 
 
     void Update()
@@ -36,7 +37,7 @@ public class ShootFireballs : MonoBehaviour
     }
 
     void Upgrade() {
-        bool upgraded = true;
+        upgraded = true;
     }
 
 
